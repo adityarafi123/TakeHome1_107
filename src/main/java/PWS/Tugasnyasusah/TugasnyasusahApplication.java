73c3ea77d772aae0a07387739c00438a8c72cb85
HP <HP@Jett>
2022-10-31 14:25:37 +0700

    FirstCommit

---
+++ src/main/java/PWS/Tugasnyasusah/TugasnyasusahApplication.java
@@ -0,0 +1,13 @@
+package PWS.Tugasnyasusah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasnyasusahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasnyasusahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PWS/Tugasnyasusah/TugasnyasusahApplicationTests.java
@@ -0,0 +1,13 @@
+package PWS.Tugasnyasusah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasnyasusahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
